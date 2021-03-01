2e958d044fadbb6f1d3da8e212d823ca0688de83
tuanzi <shenshuo@papegames.net>
2021-03-01 17:23:25 +0800

    添加 pydantic-sqlalchemy

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import re
 from io import open
 from distutils.core import setup
 
-version = '0.0.40'
+version = '0.0.41'
 
 if sys.version_info < (2, 7) or (3, 0) <= sys.version_info < (3, 6):
     print('Finit requires at least Python 2.7 or 3.6 to run.')
@@ -33,7 +33,7 @@ def get_install_requires():
     requires = ['fire==0.3.1', 'shortuuid==1.0.1', 'pymysql==0.9.3', 'sqlalchemy==1.3.0', 'python3-pika==0.9.14', 'PyJWT==1.7.1',
                 'Crypto==1.4.1', 'requests==2.24.0', 'redis==2.10.6', 'tornado>=5.0',
                 'aliyun-python-sdk-core-v3==2.13.11', 'aliyun-python-sdk-dysmsapi', 'python-dateutil==2.7.5',
-                'ldap3==2.6', 'pydantic==1.8', 'pycryptodome==3.9.9', 'rsa==4.0']
+                'ldap3==2.6', 'pydantic==1.7', 'pycryptodome==3.9.9', 'rsa==4.0']
     # if sys.platform.startswith('win'):
     #    requires.append('bottle')
     return requires
