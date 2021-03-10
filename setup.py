95d2f22e6b15551e04ef6c02c53b3406cd6f9132
ss1917 <191715030@qq.com>
2021-03-10 17:04:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_data_files():
 
 def get_install_requires():
     requires = ['fire==0.3.1', 'shortuuid==1.0.1', 'pymysql==0.9.3', 'sqlalchemy==1.3.0', 'python3-pika==0.9.14', 'PyJWT==2.0.1',
-                'Crypto==1.4.1', 'requests==2.24.0', 'redis==2.10.6', 'tornado>=6.0',
+                'Crypto==1.4.1', 'requests==2.24.0', 'redis==2.10.6', 'tornado>=6.0', 'cryptography==3.2.1',
                 'aliyun-python-sdk-core-v3==2.13.11', 'aliyun-python-sdk-dysmsapi==2.1.1', 'python-dateutil==2.7.5',
                 'ldap3==2.6', 'pydantic==1.7', 'pycryptodome==3.9.9', 'rsa==4.0']
     # if sys.platform.startswith('win'):
