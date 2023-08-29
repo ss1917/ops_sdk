b19202c84da689d1a503079631a7b75faf4cee73
shenshuo <191715030@qq.com>
2023-08-29 10:56:09 +0800

    2023年8月29日 逐步完善api 03

--- websdk2/api_set.py
+++ websdk2/api_set.py
@@ -10,11 +10,11 @@ Desc    : 解释一下吧
 
 import json
 from .tools import singleton
-from .apis import AdminAPIS, TaskAPIS, KerriganAPIS
+from .apis import AdminAPIS, TaskAPIS, KerriganAPIS, AdminV4APIS
 
 
 @singleton
-class ConstAPIS(AdminAPIS, TaskAPIS, KerriganAPIS):
+class ConstAPIS(AdminAPIS, TaskAPIS, KerriganAPIS, AdminV4APIS):
     def __init__(self):
         pass
 
