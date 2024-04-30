02436725f56fec1b4fc0859ad8f32d34c065bad6
shenshuo <191715030@qq.com>
2024-04-30 14:43:43 +0800

    fix 2024年4月18日  升级加密模块

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ desc   : CODO SDK
 import sys
 from distutils.core import setup
 
-VERSION = '0.1.65'
+VERSION = '0.1.66'
 
 if sys.version_info < (2, 7) or (3, 0) <= sys.version_info < (3, 6):
     print('This program requires at least Python 2.7 or 3.6 to run.')
@@ -26,7 +26,7 @@ def get_data_files():
 def get_install_requires():
     requires = ['fire==0.5.0', 'shortuuid==1.0.11', 'pymysql==0.9.3', 'sqlalchemy==1.3.23', 'pika==1.3.1',
                 'PyJWT==2.0.1', 'requests==2.28.2', 'redis==4.5.1', 'tornado>=6.0', 'loguru>=0.6.0',
-                'cryptography==39.0.2', 'ldap3==2.9', 'pydantic>=1.10.5']
+                'cryptography==42.0.4', 'ldap3==2.9', 'pydantic>=1.10.5']
     return requires
 
 
